Normalization of zahn, mfp fixed. threshold_kmeans_3cluster added to owntools.

bubble_stats.py
@@ -64,7 +64,7 @@ def zahn(data, xth=0.5, boxsize=100, nscales=20, upper_lim=False):
 
 	Output
 	------
-	The output is a tuple containing two numpy array: zahn_output, center.
+	The output is a tuple containing three values: radius, rdp/dr(ion), rdp/dr(neut).
 	"""
 	t1 = datetime.datetime.now()
 	if (upper_lim): 
@@ -110,16 +110,17 @@ def zahn(data, xth=0.5, boxsize=100, nscales=20, upper_lim=False):
 		radius2.append(row[0])
 		avg_ion.append(row[1])
 
-	zahn_out = np.array([radius, num_ion, num_neut, inbin, filternum])
-	center   = np.array([radius2, avg_ion])
+	#zahn_out = np.array([radius, num_ion, num_neut, inbin, filternum])
+	#center   = np.array([radius2, avg_ion])
 
 	t2 = datetime.datetime.now()
 	runtime = (t2-t1).total_seconds()/60
 
 	print "Program runtime: %f minutes." %runtime
-	print "The output is a tuple containing two numpy array: zahn_output, center."
+	print "The output is a tuple containing three values: radius, rdp/dr(ion), rdp/dr(neut)."
+	print "The curve has been normalized."
 
-	return (zahn_out, center)
+	return np.array(radius).astype(float), np.array(num_ion).astype(float), np.array(num_neut).astype(float)
 
 def mfp(data, xth=0.5, boxsize=100, iterations = 10000000, verbose=True, upper_lim=False):
 	"""
@@ -152,13 +153,14 @@ def mfp(data, xth=0.5, boxsize=100, iterations = 10000000, verbose=True, upper_l
 	else:
 		print "The data doesn't have the correct dimension"
 		return 0
-	nn = out[0]
+	nn = out[0]/iterations
 	rr = out[1]
 	t2 = datetime.datetime.now()
 	runtime = (t2-t1).total_seconds()/60
 
 	print "\nProgram runtime: %f minutes." %runtime
 	print "The output contains a tuple with three values: R dP/dr, R, Most Probable R"
+	print "The curve has been normalized."
 
 	return (nn, rr, rr[nn.argmax()])
 
@@ -211,7 +213,8 @@ def mfp_MPI(data, xth=0.5, boxsize=100, iterations = 10000000, verbose=True, upp
 
 	print "\nProgram runtime: %f minutes." %runtime
 	print "The output contains a tuple with three values: R dP/dr, R, Sizes"
+	print "The curve has been normalized."
 
-	return ht[0]*ht[1][:-1], ht[1][:-1], sizes
+	return ht[0]*ht[1][:-1]/iterations, ht[1][:-1], sizes
 
 

bubble_stats_more.py
@@ -26,7 +26,7 @@ def fof(data, xth=0.5):
 
 	return out_map, size_list
 
-def dbscan_cube(cube, eps=1.12, min_samples=7, metric='euclidean', weight=False):
+def dbscan_cube(cube, eps=1.12, min_samples=5, metric='euclidean', weight=False):
 	"""
 	DBSCAN for bubble size
 	
@@ -50,6 +50,7 @@ def dbscan_cube(cube, eps=1.12, min_samples=7, metric='euclidean', weight=False)
 	The output is a tuple containing output-map and volume-list array.
 	"""
 	t1 = datetime.datetime.now()
+
 	mn, mx = cube.min(), cube.max()
 	X = np.zeros((cube.shape[0]*cube.shape[1]*cube.shape[2],4))
 	X[:,:3] = np.argwhere(~np.isnan(cube))
@@ -59,7 +60,13 @@ def dbscan_cube(cube, eps=1.12, min_samples=7, metric='euclidean', weight=False)
 	if weight: db = DBSCAN(eps=eps, min_samples=min_samples, metric=metric).fit(X,sample_weight=X[:,-1]*(mx-mn)/cube.shape[0])
 	else: db = DBSCAN(eps=eps, min_samples=min_samples, metric=metric).fit(X)
 	labels = db.labels_
+	print "The number of clusters formed is %d." %labels.max()
+	if labels.max()<=0:
+		print "Try other values of eps and min_samples."
+		return 0, 0
+
 	out_cube = np.zeros(cube.shape)
+	sizes = np.zeros(labels.max())
 	for i in xrange(cube.shape[0]*cube.shape[1]*cube.shape[0]):
 		out_cube[X[i,0],X[i,1],X[i,2]] = labels[i]+1
 	for j in xrange(len(sizes)):

owntools.py
@@ -38,7 +38,7 @@ def threshold_kmeans(cube, upper_lim=False):
 	The input is the brightness temperature cube.
 	"""
 	array = np.zeros(cube.shape)
-	km = KMeans(n_clusters=2)
+	#km = KMeans(n_clusters=2)
 	X  = cube.reshape(-1,1)
 	y = KMeans(n_clusters=2).fit_predict(X)
 	if X[y==1].mean()>X[y==0].mean(): t_th = X[y==0].max()
@@ -48,6 +48,21 @@ def threshold_kmeans(cube, upper_lim=False):
 	print "The output contains a tuple with binary-cube and determined-threshold."
 	return array, t_th
 	
+def threshold_kmeans_3cluster(cube, upper_lim=False):
+	"""
+	The input is the brightness temperature cube.
+	"""
+	km = KMeans(n_clusters=3)
+	X  = cube.reshape(-1,1)
+	array = np.zeros(X.shape)
+	km.fit(X)
+	y = km.labels_
+	centers = km.cluster_centers_
+	if upper_lim: true_label = centers.argmin()
+	else: true_label = centers.argmax()
+	array[y==true_label] = 1
+	array = array.reshape(cube.shape)
+	return array
 
 def get_binary(array, thres, greater=False):
 	binary = np.zeros(array.shape)